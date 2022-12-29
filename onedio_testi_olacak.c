a06c222b639e46986c808625cf70cb41190a93ec
mustafaaengin <115043942+mustafaaengin@users.noreply.github.com>
2022-12-29 03:57:48 +0300

    Update onedio_testi_olacak.c

--- onedio_testi_olacak.c
+++ onedio_testi_olacak.c
@@ -4,7 +4,7 @@ int main()
 {
     int i;
     int cvp1,cvp2,cvp3,cvp4,cvp5;
-    int point1,point2,point3,point4,point5,point6,point7,point8,point9,point10;
+    int point1,point2,point3,point4,point5;
     int totalpuan;
     int a=0;
     int b=0;
@@ -12,16 +12,15 @@ int main()
     int d=0;
     int e=0;
     int f=0;
-    printf("  Testimize hosgeldiniz\n\n");
+    printf("  Hangi marvel kahramanisiniz testimize hosgeldiniz\n\n");
     
     printf("> Baslamak icin lutfen 1 e basiniz\n");
     printf("> Cikmak icin lutfen 2 ye basiniz\n");
-    
     while (a==0){
     scanf("%d", &i);
     if(i==1)
     {
-         printf("testimiz basliyor\n\n");
+         printf("Testimiz basliyor\n\n");
          a=1;
     }
     else if(i==2)
@@ -37,11 +36,11 @@ int main()
     }
     if(i==1)
     {
-         printf("1)1. soru?\n\n");
-         printf("1)cevap\n");
-         printf("2)cevap\n");
-         printf("3)cevap\n");
-         printf("4)cevap\n");
+         printf("1)Asagidakilerden birini seciniz?\n\n");
+         printf("1)Elma\n");
+         printf("2)Armut\n");
+         printf("3)Kivi\n");
+         printf("4)Musmula\n");
          
          printf("Cevabinizi giriniz : ");
          while (b==0){
@@ -71,11 +70,11 @@ int main()
             printf("Gecersiz tuslama\n\n");
             b=0;
          }}
-         printf("2)2. soru?\n\n");
-         printf("1)cevap\n");
-         printf("2)cevap\n");
-         printf("3)cevap\n");
-         printf("4)cevap\n");
+         printf("\n2)Asagidakilerden birini seciniz?\n\n");
+         printf("1)Bursa\n");
+         printf("2)Ankara\n");
+         printf("3)Istanbul\n");
+         printf("4)Bilecik\n");
          
          printf("Cevabinizi giriniz : ");
          while (c==0){
@@ -105,11 +104,11 @@ int main()
             printf("Gecersiz tuslama\n\n");
             c=0;
          }}
-         printf("3)3. soru?\n\n");
-         printf("1)cevap\n");
-         printf("2)cevap\n");
-         printf("3)cevap\n");
-         printf("4)cevap\n");
+         printf("\n3)Asgidakilerden birini seciniz\n\n");
+         printf("1)Pilav\n");
+         printf("2)Kereviz\n");
+         printf("3)Nohut\n");
+         printf("4)Portakalli Pekin ordegi\n");
          
          printf("Cevabinizi giriniz : ");
          while (d==0){
@@ -139,11 +138,11 @@ int main()
             printf("Gecersiz tuslama\n\n");
             d=0;
          }}
-         printf("4)4. soru?\n\n");
-         printf("1)cevap\n");
-         printf("2)cevap\n");
-         printf("3)cevap\n");
-         printf("4)cevap\n");
+         printf("\n4)Asgidakilerden birini seciniz\n\n");
+         printf("1)Para\n");
+         printf("2)Ask\n");
+         printf("3)Guven\n");
+         printf("4)Bilgisayar Muhendisligi\n");
          
          printf("Cevabinizi giriniz : ");
          while (e==0){
@@ -173,11 +172,11 @@ int main()
             printf("Gecersiz tuslama\n\n");
             e=0;
          }}
-         printf("5)5. soru?\n\n");
-         printf("1)cevap\n");
-         printf("2)cevap\n");
-         printf("3)cevap\n");
-         printf("4)cevap\n");
+         printf("\n5)Asgidakilerden birini seciniz\n\n");
+         printf("1)Yaz\n");
+         printf("2)Sonbahar\n");
+         printf("3)ilkbahar\n");
+         printf("4)Ankara ayazi\n");
          
          printf("Cevabinizi giriniz : ");
          while (f==0){
@@ -210,52 +209,52 @@ int main()
              totalpuan = point1 + point2 + point3 + point4 + point5;
 
     if(totalpuan==5){
-        printf("5 puana ne karsilik geliyosa artik");
+        printf("Hulk");
     }
     else if(totalpuan==6){
-        printf("6 puana ne karsilik geliyosa artik");
+        printf("Gamora");
     }
     else if(totalpuan==7){
-        printf("7 puana ne karsilik geliyosa artik");
+        printf("Winter Soldier");
     }
     else if(totalpuan==8){
-        printf("8 puana ne karsilik geliyosa artik");
+        printf("Black Panter");
     }
     else if(totalpuan==9){
-        printf("9 puana ne karsilik geliyosa artik");
+        printf("Spiderman");
     }
     else if(totalpuan==10){
-        printf("10 puana ne karsilik geliyosa artik");
+        printf("Deadpool");
     }
     else if(totalpuan==11){
-        printf("11 puana ne karsilik geliyosa artik");
+        printf("Doctor Strange");
     }
     else if(totalpuan==12){
-        printf("12 puana ne karsilik geliyosa artik");
+        printf("Groot");
     }
     else if(totalpuan==13){
-        printf("13 puana ne karsilik geliyosa artik");
+        printf("Iron Man");
     }
     else if(totalpuan==14){
-        printf("14 puana ne karsilik geliyosa artik");
+        printf("Captain Marvel");
     }
     else if(totalpuan==15){
-        printf("15 puana ne karsilik geliyosa artik");
+        printf("Hawkeye");
     }
     else if(totalpuan==16){
-        printf("16 puana ne karsilik geliyosa artik");
+        printf("Wolverine");
     }
     else if(totalpuan==17){
-        printf("17 puana ne karsilik geliyosa artik");
+        printf("Lionel MESSI");
     }
     else if(totalpuan==18){
-        printf("18 puana ne karsilik geliyosa artik");
+        printf("Thor");
     }
     else if(totalpuan==19){
-        printf("19 puana ne karsilik geliyosa artik");
+        printf("Captain America");
     }
     else if(totalpuan==20){
-        printf("20 puana ne karsilik geliyosa artik");
+        printf("Scarlet Witch");
     }
      return 0;
 }
